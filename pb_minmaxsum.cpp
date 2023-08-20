fa9b30c174339332236aecd2d3ae5440f40aa9c7
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-08-20 16:51:29 +0530

    Add files via upload

---
+++ pb_minmaxsum.cpp
@@ -0,0 +1,23 @@
+//problem solving- Hacker Rank Min-Max Sum
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int val, i;
+	long long int sum = 0;
+	vector<int> array;
+	for (i = 0; i < 5; i++)
+	{
+		//cout<<"enter the values:";
+		cin >> val;
+		sum += val;
+		//cout<<sum<<" ";
+		array.push_back(val);
+	}
+
+	//cout<<endl;
+	sort(array.begin(), array.end());
+	
+	cout << sum - array[4] << " " << sum - array[0];
+	return 0;
+}
