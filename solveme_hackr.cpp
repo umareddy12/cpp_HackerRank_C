e2ab204abd3527edb3c1f620ddf209b68cd13b56
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-08-20 11:04:58 +0530

    Add files via upload

---
+++ solveme_hackr.cpp
@@ -0,0 +1,25 @@
+//Hacker rank Solve me first 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+ return a+b;
+  
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cout<<"enter two numbers:";
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<"the sum:";
+  cout<<sum;
+  return 0;
+}
+
