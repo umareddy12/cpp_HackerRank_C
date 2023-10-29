1cf772a05c4aed2c7983d7ca47e97c4a32b0342a
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-29 11:17:10 +0530

    CCprblm1.c

---
+++ CCprblm1.c
@@ -0,0 +1,18 @@
+//Code Chef problem:Storing Boxes in c 
+#include <stdio.h>
+
+int main() {
+
+  int length = 11;
+  int width = 13;
+
+  printf("%d\n", length * width);
+  printf("%d\n", 2 * (length + width));
+
+  width = 15;          
+
+  printf("%d\n", length * width);
+  printf("%d\n", 2 * (length + width));
+  
+  return 0;
+}
