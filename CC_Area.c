7222ada670d89f4efb22c7279af08393fe6bbbd8
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-29 11:26:40 +0530

    CC_Area.c

---
+++ CC_Area.c
@@ -0,0 +1,12 @@
+//Code Chef problem:Area of Rectanglein C
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int length=45;
+	int width=76;
+	int area=length*width;
+	printf("The area of the given rectangle is %d",area);
+	return 0;
+}
+
