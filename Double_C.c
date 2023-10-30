07a508a2c3b970545c529753ab6d197af923295f
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-30 20:21:07 +0530

    Double_C.c

---
+++ Double_C.c
@@ -0,0 +1,12 @@
+//Code chef problem:Double datatype in c
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	double pi=3.14;
+double radius=8.9;
+double area=pi*radius*radius;
+printf("The Area of the given Circle is %f",area);
+	return 0;
+}
+
