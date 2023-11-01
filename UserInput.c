a8e805766e629d936c44c91d949c2606e29843ab
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-01 19:16:27 +0530

    UserInput.c

---
+++ UserInput.c
@@ -0,0 +1,14 @@
+//Code Chef problem:Taking user input in C
+
+
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	scanf("%d",&a);
+	printf("Your number is: %d",a);
+	return 0;
+}
+
