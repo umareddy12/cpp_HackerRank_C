fa99b40503d5e57e2157a7d665222e7e0463176e
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-01 18:38:37 +0530

    Strlen.c

---
+++ Strlen.c
@@ -0,0 +1,12 @@
+//Code Chef problem:String length in C
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char txt[]="NumeroTres";
+	int a=strlen(txt);
+	printf("The length of the word is: %d",a);
+	return 0;
+}
+
