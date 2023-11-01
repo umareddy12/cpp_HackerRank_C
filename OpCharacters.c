b94d8e3d6c4bd0d1c4629fc16e8c89dbe4f6f3e6
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-01 18:47:07 +0530

    OpCharacters.c

---
+++ OpCharacters.c
@@ -0,0 +1,12 @@
+//Code Chef problem:Outputting Characters from a String in C
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char word[]="Programming";
+	printf("%c\n",word[2]);
+	printf("%c",word[4]);
+	return 0;
+}
+
