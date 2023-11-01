3a1846159cab358f98a136b6a9ccd4320bff4875
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-01 18:34:36 +0530

    StrInt.c

---
+++ StrInt.c
@@ -0,0 +1,13 @@
+//Code Chef problem:Strings and Integers in C
+
+#include <stdio.h>
+ 
+int main () {
+
+  char num1[] = "25";
+  char num2[] = "69";
+  strcat(num1,num2);
+  printf("%s", num1);
+
+  return 0;
+}
