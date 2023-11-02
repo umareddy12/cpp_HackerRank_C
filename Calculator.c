eb13593c9d30ef1e065fc88752d123144453509c
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-02 10:10:12 +0530

    Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,15 @@
+//Code Chef proble:Calculator in C
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	int b;
+	scanf("%d%d",&a,&b);
+	int sum=a+b;
+	int diff=a-b;
+	printf("Sum is: %d\n",sum);
+	printf("Difference is: %d",diff);
+	return 0;
+}
+
