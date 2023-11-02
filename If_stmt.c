07553288fcc0427e38a34ad7cef19b9c666a0dd4
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-02 10:29:04 +0530

    If_stmt.c

---
+++ If_stmt.c
@@ -0,0 +1,16 @@
+//Code Chef problrm:If Statement in C
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	int b;
+	scanf("%d\n%d",&a,&b);
+	if(a>b)
+	{
+	    printf("Coding is Fun!");
+	}
+	return 0;
+}
+
