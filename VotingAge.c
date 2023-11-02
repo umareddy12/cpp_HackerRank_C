da27bef7541729eefeefbcdfadd60a0f28c187ae
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-02 10:20:40 +0530

    VotingAge.c

---
+++ VotingAge.c
@@ -0,0 +1,18 @@
+//code chef problem :If & Else Statements in C
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int age=25;
+	int vage=18;
+	if(age>=vage)
+	{
+	    printf("Old enough to vote!");
+	}
+	else{
+	    printf("Not old enough to vote.");
+	}
+	return 0;
+}
+
