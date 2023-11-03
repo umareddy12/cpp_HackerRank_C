6f4149f8b5779d0109fb08020db15edff256e757
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-03 09:16:28 +0530

    Else_If.c

---
+++ Else_If.c
@@ -0,0 +1,28 @@
+//code chef problem :Else If Statement in C
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int b=23;
+	int r=45;
+	if(r>b)
+	{
+	    printf("Rob Scored higher marks than Bob.\n");
+	}
+	else{
+	    printf("Bob & Rob both scored the same\n");
+	}
+	    b=15;
+	    r=15;
+	    if(r>b)
+	    {
+	        printf("Rob Scored higher marks than Bob.\n");
+	        
+	    }
+	    else{
+	        printf("Bob & Rob both scored the same");
+	    }
+	return 0;
+}
+
