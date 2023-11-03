62f84dc8d986bf9c59d61e758c727969d4cd3d36
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-03 10:32:53 +0530

    Display_count.c

---
+++ Display_count.c
@@ -0,0 +1,10 @@
+//code chef problem:Displaying the count of elements in C
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int myNumbers[6]={10, 20, 30, 40, 50, 60};
+	printf("%d",sizeof(myNumbers)/4);
+	return 0;
+}
+
