33dbe55779d9c36a5b10f9efce8a29a1184380b7
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-03 09:38:56 +0530

    Condition.c

---
+++ Condition.c
@@ -0,0 +1,15 @@
+//code chef problem:if condition in C
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n > 5) {
+        printf("the number is greater than 5");
+    } else {
+        printf("the number is smaller than or equal to 5");
+    }
+
+    return 0;
+}
