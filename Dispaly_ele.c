88ead1b9563f8cbc5fd90f92237556575806f7d7
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-03 10:23:54 +0530

    Dispaly_ele.c

---
+++ Dispaly_ele.c
@@ -0,0 +1,11 @@
+//code chef problem:Displaying elements in C
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char week[4][10]={"Monday", "Tuesday", "Wednesday", "Thursday"};
+	printf("%s\n",week[2]);
+	printf("%s",week[3]);
+	return 0;
+}
+
