2bf265e2fb89dbfba984e9eef7432a81d9abe096
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-03 10:45:37 +0530

    While_c.c

---
+++ While_c.c
@@ -0,0 +1,14 @@
+//code chef problem:While Loop in C
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a=0;
+	while(a<7)
+	{
+	    printf("%d\n",a);
+	a=a+1;
+	}
+	return 0;
+}
+
