320352551178b9864a67b7192ae41b47b581ea08
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-05 10:40:07 +0530

    Do_While.c

---
+++ Do_While.c
@@ -0,0 +1,17 @@
+//code chef problem:Do While Loop in C
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i1=0;
+	do{
+	    printf("%d\n",i1);
+	     i1=i1+1;
+	
+	}
+	while(i1<6);
+
+	return 0;
+}
+
