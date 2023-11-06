b75f34e843a4bef3e028d41deefd54e85bf55ed9
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-11-06 14:49:24 +0530

    Add files via upload

---
+++ Watermelon1.cpp
@@ -0,0 +1,18 @@
+//Codeforces:Watermelon problem
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int w;//declaration
+	//cout<<"enter weight:";
+	cin>>w;
+	if(w%2==0 && w>2){
+	//condition to check even or odd	
+	cout<<"YES"<<endl;	
+	}
+	else{
+	cout<<"NO"<<endl;
+	}	
+	return 0;
+}
+
